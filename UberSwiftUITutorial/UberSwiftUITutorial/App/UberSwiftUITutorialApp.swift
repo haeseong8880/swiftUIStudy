8a1e51a6202a57846b4039965d894af8b15eccf3
Haeseong <hesung0805@gmail.com>
2022-12-04 18:11:30 +0900

    first commit Uber clone coding

---
+++ UberSwiftUITutorial/UberSwiftUITutorial/App/UberSwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberSwiftUITutorialApp.swift
+//  UberSwiftUITutorial
+//
+//  Created by haeseongJung on 2022/12/04.
+//
+
+import SwiftUI
+
+@main
+struct UberSwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
---
+++ UberSwiftUITutorial/UberSwiftUITutorial/Managers/LocationManager.swift
@@ -0,0 +1,28 @@
+//
+//  LocationManager.swift
+//  UberSwiftUITutorial
+//
+//  Created by haeseongJung on 2022/12/04.
+//
+
+import CoreLocation
+
+
+class LocationManager: NSObject, ObservableObject {
+    private let locationManager = CLLocationManager()
+    
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+}
+
+extension LocationManager: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard !locations.isEmpty else { return }
+        locationManager.startUpdatingLocation()
+    }
+}
